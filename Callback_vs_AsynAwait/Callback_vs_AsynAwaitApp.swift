19c2f68d086cb9323f9520df88a2eda66afb9594
EstrHuP <estrhup@gmail.com>
2023-07-18 18:57:13 +0200

    first commit

---
+++ Callback_vs_AsynAwait/Callback_vs_AsynAwaitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Callback_vs_AsynAwaitApp.swift
+//  Callback_vs_AsynAwait
+//
+//  Created by EstrHuP on 18/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Callback_vs_AsynAwaitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Callback_vs_AsynAwait/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Callback_vs_AsynAwait
+//
+//  Created by EstrHuP on 18/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
